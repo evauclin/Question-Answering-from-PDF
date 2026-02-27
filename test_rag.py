512fdde5f473807908ad43667c3c2f0cf583f06d
rmurat <rony.murat@gmail.com>
2026-02-27 17:29:50 -0400

    fix import

--- test_rag.py
+++ test_rag.py
@@ -1,5 +1,5 @@
 from query_data import query_rag
-from langchain.llms.ollama import Ollama
+from langchain_community.llms.ollama import Ollama
 
 EVAL_PROMPT = """
 Expected Response: {expected_response}
